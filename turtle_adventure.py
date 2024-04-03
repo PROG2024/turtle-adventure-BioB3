2677f9c3ec0557da6f08bf8e1c7c32d7420b3030
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-04-03 11:38:08 +0700

    add ChasingEnemy

--- turtle_adventure.py
+++ turtle_adventure.py
@@ -349,19 +349,14 @@ class RandomEnemy(Enemy):
     def update_x(self):
         if self.__destination[0] > self.x:
             self.x += min(self.speed, self.__destination[0] - self.x)
-        elif self.__destination[0] < self.x:
-            self.x -= min(self.speed, self.x - self.__destination[0])
         else:
-            return
+            self.x -= min(self.speed, self.x - self.__destination[0])
 
     def update_y(self):
         if self.__destination[1] > self.y:
             self.y += min(self.speed, self.__destination[1] - self.y)
-        elif self.__destination[1] < self.y:
-            self.y -= min(self.speed, self.y - self.__destination[1])
         else:
-            return
-
+            self.y -= min(self.speed, self.y - self.__destination[1])
     def create(self) -> None:
         self.__id = self.canvas.create_oval(0, 0, 0, 0,
                                        fill=self.color)
@@ -390,6 +385,43 @@ class ChasingEnemy(Enemy):
                  size: int,
                  color: str):
         super().__init__(game, size, color)
+        self.__player_loc = self.gen_player_loc()
+
+    def gen_player_loc(self):
+        return (self.game.player.x, self.game.player.y)
+
+    def update_x(self):
+        if self.__player_loc[0] > self.x:
+            self.x += min(self.speed, self.__player_loc[0] - self.x)
+        else:
+            self.x -= min(self.speed, self.x - self.__player_loc[0])
+
+    def update_y(self):
+        if self.__player_loc[1] > self.y:
+            self.y += min(self.speed, self.__player_loc[1] - self.y)
+        else:
+            self.y -= min(self.speed, self.y - self.__player_loc[1])
+
+    def create(self) -> None:
+        self.__id = self.canvas.create_oval(0, 0, 0, 0,
+                                       fill=self.color)
+
+    def update(self) -> None:
+        self.__player_loc = self.gen_player_loc()
+        self.update_x()
+        self.update_y()
+        if self.hits_player():
+            self.game.game_over_lose()
+
+    def render(self) -> None:
+        self.canvas.coords(self.__id,
+                           self.x-self.size/2,
+                           self.y-self.size/2,
+                           self.x+self.size/2,
+                           self.y+self.size/2)
+
+    def delete(self) -> None:
+        self.canvas.delete(self.__id)
         
 # TODO
 # Complete the EnemyGenerator class by inserting code to generate enemies
@@ -430,11 +462,11 @@ class EnemyGenerator:
         """
         Create a new enemy, possibly based on the game level
         """
-        for _ in range(10):
-            new_enemy = RandomEnemy(self.__game, 20, "red")
-            new_enemy.x = random.randint(0, new_enemy.canvas.winfo_width())
-            new_enemy.y = random.randint(0, new_enemy.canvas.winfo_height())
-            self.game.add_element(new_enemy)
+        # for _ in range(10):
+        new_enemy = ChasingEnemy(self.__game, 20, "red")
+        new_enemy.x = random.randint(0, new_enemy.canvas.winfo_width())
+        new_enemy.y = random.randint(0, new_enemy.canvas.winfo_height())
+        self.game.add_element(new_enemy)
 
 
 class TurtleAdventureGame(Game): # pylint: disable=too-many-ancestors
